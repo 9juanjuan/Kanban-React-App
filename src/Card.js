3d25a191b316e0a793481cda70ef1190a86d55eb
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-04-30 21:19:39 -0400

    adds event handler onClick function and binds function

--- src/Card.js
+++ src/Card.js
@@ -9,20 +9,25 @@ class Card extends Component {
             showDetails: false
         };
     }
+    toggleDetails() {
+        this.setState({
+            showDetails: !this.state.showDetails});
+    }
 render() {
     let cardDetails;
     if (this.state.showDetails) {
         cardDetails = (
             <div className="card__details">
-            {this.props.description}
-            <CheckList cardId={this.props.id} tasks={this.props.tasks} /></div>
+                {this.props.description}
+                <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+            </div>
         );
     }
         return (
             <div className="card">
-                <div className="card__title" onClick={
-                    ()=>this.setState({showDetails: !this.state.showDetails})
-                }>{this.props.title}</div>
+                <div className="card__title" onClick={this.toggleDetails.bind(this)}>
+                  {this.props.title}
+                </div>
                 {cardDetails}
                 </div>
         );
